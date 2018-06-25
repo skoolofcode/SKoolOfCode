0705f686c54a3cce62122654cfa369dd563da32a
ishitajain21 <40191155+ishitajain21@users.noreply.github.com>
2018-06-25 14:20:04 -0700

    This answers your question using a crystal ball.

---
+++ TrailBlazers/LiveClass/Ishita/Practice/Magic8ball.py
@@ -0,0 +1,14 @@
+import random
+a = int(random.randint(0,1))
+
+print("Welcome to the crystal ball or the magic 8 ball. Ask a question about the future. Find out the result.")
+b = input("Ask your question now.Make sure it is answered by a yes or no. The crystal ball is hazy. It is hard to interpret.")
+
+
+
+if a == 0 :
+    print("\nNo.")
+if a == 1 :
+    print("\nYes.")
+
+print("There is your answer. Now that you know, you could try to change it. But remember, some things in your destiny are meant to be. By changing it, you might be fulfiling it. Will you take the risk? ")
