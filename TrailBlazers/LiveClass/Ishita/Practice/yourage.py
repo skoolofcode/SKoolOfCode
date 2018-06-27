7551e98314d7fcbba26e15f90276ba386fa84716
ishitajain21 <40191155+ishitajain21@users.noreply.github.com>
2018-06-26 17:32:52 -0700

    It tells you what year you will be  certain age.

---
+++ TrailBlazers/LiveClass/Ishita/Practice/yourage.py
@@ -0,0 +1,10 @@
+print(" I am going to take your name and age and tell you the exact year you will turn the age you tell me.")
+
+name = input("What is your name?")
+age = int(input("What is your age?"))
+year = int(input("What age do you want me to find out?"))
+
+sub = 2018-age
+ans = sub + year
+
+print(name, "will be", year, "years old in the year",ans)
