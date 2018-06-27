b31750f5968c30fa667971caab159096bac2ebc6
ishitajain21 <40191155+ishitajain21@users.noreply.github.com>
2018-06-26 17:54:27 -0700

    It gives you the year when you turn a certain age

--- TrailBlazers/LiveClass/Ishita/Practice/yourage.py
+++ TrailBlazers/LiveClass/Ishita/Practice/yourage.py
@@ -3,8 +3,9 @@ print(" I am going to take your name and age and tell you the exact year you wil
 name = input("What is your name?")
 age = int(input("What is your age?"))
 year = int(input("What age do you want me to find out?"))
+now = int(input("What year is it now?"))
 
-sub = 2018-age
+sub = now-age
 ans = sub + year
 
 print(name, "will be", year, "years old in the year",ans)
