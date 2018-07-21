382142b0279e397f175cab5441a23ab81573f153
PrishaSood <prishasood23@gmail.com>
2018-07-20 19:08:45 -0700

    prisha commit

---
+++ TrailBlazers/Prisha/Lesson1PrintStatements.py
@@ -0,0 +1,4 @@
+# Lesson 1: print() function
+print('Hello, Buzz Coder! My name is PRISHA')
+print()
+print('Hello, CodeCraft!Hi my name is Prisha')
---
+++ TrailBlazers/Prisha/nSum.py
@@ -0,0 +1,5 @@
+while true:
+	n=input (" enter a number ")
+	x=0
+	for i in range (1, n+1):
+		print (1+2+x == n)
