9edc9fc9ac63ff3f2898e637cc704bbae968a120
ishitajain21 <40191155+ishitajain21@users.noreply.github.com>
2018-07-24 11:15:44 -0700

    Cost of the candies, candies per roll number used.

---
+++ TrailBlazers/LiveClass/Ishita/Test/squaredcandies.py
@@ -0,0 +1,13 @@
+x = 0
+
+for number in range(1,542,1):
+    print("Roll number is ", number)
+    can = number * number
+    print("This is how many candies roll", number,"will have",can)
+    global x 
+    y = x + can
+    x = y
+    
+    
+cost = x * 0.10
+print("The candies will cost", cost)
