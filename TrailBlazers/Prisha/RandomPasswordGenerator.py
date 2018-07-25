53dfe289a9d0fa1c947102815b770ab2fda212fe
PrishaSood <40643833+PrishaSood@users.noreply.github.com>
2018-07-25 13:34:02 -0700

    Delete RandomPasswordGenerator.py

--- TrailBlazers/Prisha/RandomPasswordGenerator.py
+++
@@ -1,6 +0,0 @@
-from random import randint 
-range = int (input (" How long do you want your password to be (between 1- 10 ) "))
-A= random.choice ("1,2,3,4,5,6,7,8,9,0")
-randrange = randint (range , A)
-if range==1:
-    print (randrange)
