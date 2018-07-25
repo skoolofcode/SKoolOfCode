da49f6e7c4f2252ffd6f7b58b8ff8aacbfea9716
ishitajain21 <40191155+ishitajain21@users.noreply.github.com>
2018-07-25 09:25:43 -0700

    Tells the sum of all the numbers before it and this one.

---
+++ TrailBlazers/LiveClass/Ishita/Test/seriessum.py
@@ -0,0 +1,12 @@
+num = int(input("WHat is the number you would like me to tell you the sum of?"))
+
+x = 0
+n = num+1
+for i in range(1,n):
+    ns = i
+    global x
+    y = x + ns
+    x = y
+    
+
+print(x)
