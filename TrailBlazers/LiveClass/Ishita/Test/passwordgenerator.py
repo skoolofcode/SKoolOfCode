082c1ce57a43febf04c97b6438d327b51ddbeec2
ishitajain21 <40191155+ishitajain21@users.noreply.github.com>
2018-07-26 04:11:05 +0000

    Update passwordgenerator.py

--- TrailBlazers/LiveClass/Ishita/Test/passwordgenerator.py
+++ TrailBlazers/LiveClass/Ishita/Test/passwordgenerator.py
@@ -1,20 +1,7 @@
-import string
-import random
-import getpass
-
 print("Hi! We all know how difficult it is to set up passwords, especially when we want it to be for a secure account. A password which is secure. No worries! I will give you a password and a key number if you ever lose it. Enjoy!")
 print("P.S. DOn't worry, I don't hack into your account.")
 name = input("What is your name?")
-choice = int(input("DO you want to make a new password(1) or store your passwords(2) or retreive a password(3)?"))
 
-if choice == 1:
-    maker()
-else:
-    if choice == 2:
-        store()
-    else:
-        if choice == 3:
-            get()
 def maker():
     num = input("How many digits would you like in your password.Type in the 'a' the number of characters you want your password to be")
 
