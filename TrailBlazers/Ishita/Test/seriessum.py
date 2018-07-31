49c78e4f87da2037bd9d0b9c70ea784379e9a317
ishitajain21 <40191155+ishitajain21@users.noreply.github.com>
2018-07-31 11:52:09 -0700

    Update seriessum.py

--- TrailBlazers/Ishita/Test/seriessum.py
+++ TrailBlazers/Ishita/Test/seriessum.py
@@ -1,15 +1,11 @@
-#SKC : Good job with the logic.
-#SKC : Dont use global. Write functions. Use return.
+def ss():
+    num = int(input("WHat is the number you would like me to tell you the sum of from 1 to that number?"))
+    x = 0
+    n = num+1
+    for i in range(1,n):
+        ns = i
+        y = x + ns
+        x = y
 
-num = int(input("WHat is the number you would like me to tell you the sum of?"))
-
-x = 0
-n = num+1
-for i in range(1,n):
-    ns = i
-    global x
-    y = x + ns
-    x = y
-    
-
-print(x)
+    print(x)
+ss()
