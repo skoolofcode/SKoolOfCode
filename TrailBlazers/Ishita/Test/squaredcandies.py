75fd4cce9002a84d7a0ea390b6f3d2db1c5a2993
ishitajain21 <40191155+ishitajain21@users.noreply.github.com>
2018-07-31 12:01:55 -0700

    Update squaredcandies.py

--- TrailBlazers/Ishita/Test/squaredcandies.py
+++ TrailBlazers/Ishita/Test/squaredcandies.py
@@ -1,15 +1,11 @@
-#SKC : Good job with the logic.
-#SKC : Dont use global. Write functions for squaring and cost. Use return.
 x = 0
-
 for number in range(1,542,1):
     print("Roll number is ", number)
     can = number * number
     print("This is how many candies roll", number,"will have",can)
-    global x 
     y = x + can
     x = y
-    
-    
+
 cost = x * 0.10
+
 print("The candies will cost", cost)
