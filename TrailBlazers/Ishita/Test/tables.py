0136a6d9ad294b8b5bd794a1fd70e43ab6e885c6
ishitajain21 <40191155+ishitajain21@users.noreply.github.com>
2018-07-31 12:09:48 -0700

    Update tables.py

--- TrailBlazers/Ishita/Test/tables.py
+++ TrailBlazers/Ishita/Test/tables.py
@@ -1,8 +1,10 @@
 #SKC : Good job with the logic.
 #SKC : Dont use global. Write functions. Use return. Print outside the function
 
-num = int(input("What is the number you want me to make a table of? "))
+def tables():
+    num = int(input("What is the number you want me to make a table of? "))
 
-for i in range(1,11):
+    for i in range(1,11):
     k = num*i
     print(num,"*",i,"=",k )
+tabels()
