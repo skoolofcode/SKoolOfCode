deb3571a5907a48809f39bb4c23b1eb3db240b9b
Maanya Arora <40130525+Maanya-Arora@users.noreply.github.com>
2018-08-02 13:23:27 -0700

    Timestablegenerator.py
    Times Table Generator Updated

--- TrailBlazers/maanya/test/Timestablegenerator.py
+++ TrailBlazers/maanya/test/Timestablegenerator.py
@@ -2,7 +2,7 @@
 #SKC : Consider printing in a table format
 def timestablecode(n):
 	for i in range(1,11):
-		print(i,'*',n,'=' n * i)
+		print(i,'*',n,'=', n * i)
 	return
 n = int(input("Enter a number and I will give you it's multiplication table:"))
 timestablecode(n)
