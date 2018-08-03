4f59a59d7133064e21864268a927af3430909f0f
Maanya Arora <40130525+Maanya-Arora@users.noreply.github.com>
2018-08-03 13:05:35 -0700

    Maanya's Series Summer -Updated

--- TrailBlazers/maanya/test/seriessummer.py
+++ TrailBlazers/maanya/test/seriessummer.py
@@ -1,11 +1,13 @@
 #SKC : Great Job! Should work great.
 #SKC : Functions should return their result so that other can use it. Consider that and return 
 # rather than the value inside the function i.e. can u print the final results from out of function.
+#Maanya: SKC, Something like this: // I can not print 'totalsum' outside function because I would have to use Global Variables
 
-def seriessum():
-	summer = int(input("Hello I am The Series Sum Bot. Enter a number which you would like me to sum for you today:"))
+def seriessum(summer):
 	totalsum = 0
 	for i in range(1,summer + 1):
 		totalsum = totalsum + i 
-	print(totalsum)
+		print(totalsum)
+		return
+summer = int(input("Hello I am The Series Sum Bot. Enter a number which you would like me to sum for you today:"))
 seriessum()
