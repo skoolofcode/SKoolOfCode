9e34646644c85fbc396bb609ced6a6d5c6d78a35
Maanya Arora <40130525+Maanya-Arora@users.noreply.github.com>
2018-08-03 13:57:05 -0700

    Maanya's Squared Candies -Half-updated

--- TrailBlazers/maanya/test/squaredcandies.py
+++ TrailBlazers/maanya/test/squaredcandies.py
@@ -7,7 +7,12 @@ def sqcandies():
 	for i in range(0,542):
 		rollcandies = i * i
 		totalcandies = totalcandies + rollcandies
-	totalcost = round(totalcandies * 0.10,2)
 	print("The total number of candies Mr.Tom needs is",totalcandies)
-	print("The total cost of",totalcandies,"candies is $",totalcost)
+	return
 sqcandies()
+
+def candycost():
+    totalcost = round(totalcandies * 0.10,2)
+    print("The total cost of",totalcandies,"candies is $",totalcost)
+    return
+candycost()
