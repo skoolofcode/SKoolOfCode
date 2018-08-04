42ef9f525b17cac30ebc41961d058ce28c7bf5c7
PrishaSood <prishasood23@gmail.com>
2018-08-04 00:09:47 -0700

    new CodeCraft Code coming in

---
+++ TrailBlazers/Prisha/Practice/stringManipulation.py
@@ -0,0 +1,7 @@
+
+name = input(" what is your name")
+favoritenumber = input(" Hi, " + name + " what is your favorite number")
+greeting = name + "'s favorite number is " + str(favoritenumber)
+print (greeting)
+
+    
--- TrailBlazers/Prisha/Test-1/SeriesSum.py
+++ TrailBlazers/Prisha/Test-1/SeriesSum.py
@@ -1,7 +1,7 @@
 #SKC : Good job!
 #SKC : Why while ( 9>sum )? Can u replace this with the right way to do the infinity loop 
 
-
+sum = 0 
 while True:
 	N = int (input (" enter a positive number "))
 	for i in range (1, N+1):
