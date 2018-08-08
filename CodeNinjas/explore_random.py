d81cecdade77a68b79f3982551d23b902476bb05
Prisha <Prishasood23@gmail.com>
2018-08-07 22:28:45 -0700

    random sample

---
+++ CodeNinjas/explore_random.py
@@ -0,0 +1,6 @@
+import random
+
+print("I want to print a random decimal", random.random())
+print("I want to print a random number" , random.randint(4,90))
+
+print("I want to print a random character",random.choice("abcdefghijklmnop"))
