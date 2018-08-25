cd43ac706ca45d3ab13bec6114f4ba6189ea7ba6
ishitajain21 <40191155+ishitajain21@users.noreply.github.com>
2018-08-25 09:32:19 -0700

    Update stepone.py

--- TrailBlazers/Ishita/Minecraft/stepone.py
+++ TrailBlazers/Ishita/Minecraft/stepone.py
@@ -1,5 +1,15 @@
 from mcpi.minecraft import Minecraft
 mc = Minecraft.create()
-mc.postToChat("Hello Student! Welcome to my Minecraft version!")
+stone = 1
+import time
+
+mc.postToChat("Hello Player! Welcome to my Minecraft version! You will try to find these blocks when I will teleport you every 20 seconds.")
 x, y, z = mc.player.getPos()
-mc.player.setPos(x, y+100, z)
+mc.setBlocks(x+1, y+1, z+1, x+11, y+11, z+11, stone)
+
+while True:
+  mc.player.setPos(x+10, y+150, z+5) 
+  time.sleep(60)
+      
+
+
