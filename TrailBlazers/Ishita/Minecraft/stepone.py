f8904f2d6fd75ec38c4ea0602494905b04b916f3
ishitajain21 <40191155+ishitajain21@users.noreply.github.com>
2018-08-28 20:20:49 +0000

    Update stepone.py

--- TrailBlazers/Ishita/Minecraft/stepone.py
+++ TrailBlazers/Ishita/Minecraft/stepone.py
@@ -5,11 +5,14 @@ import time
 
 mc.postToChat("Hello Player! Welcome to my Minecraft version! You will try to find these blocks when I will teleport you every 20 seconds.")
 x, y, z = mc.player.getPos()
-mc.setBlocks(x+1, y+1, z+1, x+11, y+11, z+11, stone)
+mc.setBlocks(x +1, y+1, z+1, x+1, y+1, z+1, stone)
 
 while True:
-  mc.player.setPos(x+10, y+150, z+5) 
-  time.sleep(60)
+  mc.player.setPos(x+100, y+150, z+500) 
+  a,b,c = mc. player.getPos()
+  time.sleep(20)
+  if x,y,z == a,b,c:
+    mc.postToChat("Well done! You have won!!!")
       
 
 
