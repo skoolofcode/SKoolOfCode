6e8d3ae1a8cda2019d2c763c8a9686df2ba1819d
ishitajain21 <40191155+ishitajain21@users.noreply.github.com>
2018-08-29 20:08:01 +0000

    Update stepone.py

--- TrailBlazers/Ishita/Minecraft/stepone.py
+++ TrailBlazers/Ishita/Minecraft/stepone.py
@@ -5,11 +5,12 @@ import time
 
 mc.postToChat("Hello Player! Welcome to my Minecraft version! You will try to mke a structure in 20 seconds. Then I will teleport you. You have to try to find the structure that you built.") 
 mc.postToChat("WHen you have chosen the location you want to build with, type 1.")
-m = input("Now don't you move move once you have typed 1. ")
+mc.postToCHat("GO quickly to find the position where you will build! You have exactoly 10 seconds!")
 
-if m == "1":
-  pos = mc.player.getPos()
-  
+time.sleep(10)
+
+pos = mc.player.getPos()
+mc.postToChat("Allright! 20 seconds to build!") 
 time. sleep(20)
 mc.player.setPos(pos + 100)
 
