5c6e75fbe77418152c387b0cf438a21aad400e2f
ishitajain21 <40191155+ishitajain21@users.noreply.github.com>
2018-08-29 20:27:17 +0000

    Update stepone.py

--- TrailBlazers/Ishita/Minecraft/stepone.py
+++ TrailBlazers/Ishita/Minecraft/stepone.py
@@ -1,16 +1,16 @@
 from mcpi import minecraft
 mc = minecraft.Minecraft.create()
-stone = 1
+
 import time
 
 mc.postToChat("Hello Player! Welcome to my Minecraft version! You will try to mke a structure in 20 seconds. Then I will teleport you. You have to try to find the structure that you built.") 
-mc.postToCHat("GO quickly to find the position where you will build! You have exactoly 10 seconds!")
+mc.postToChat("GO quickly to find the position where you will build! You have exactoly 10 seconds!")
 time.sleep(10)
 x,y,z = mc.player.getPos()
 
-mc.postToChat("Allright! 20 seconds to build!") 
-time. sleep(20)
-mc.player.setPos(x + 10,y + 100, z +9)
+mc.postToChat("Allright! 10 seconds to build!") 
+time. sleep(10)
+mc.player.setPos(x + 100,y + 100, z +100)
 
 while True:
   time.sleep(10)
