92a58cffc768a7fd3d3bce9307ea804a8f3401ea
PrishaSood <prishasood23@gmail.com>
2018-09-03 18:14:54 -0700

    fixing issue

--- TrailBlazers/Prisha/Practice/AmIOldEnoughToVote.py
+++ TrailBlazers/Prisha/Practice/AmIOldEnoughToVote.py
@@ -1,6 +1,7 @@
-age= int (input (print (" what is your age ")) 
-if age >= 18:
-	print (" you can vote")
+
+age= int (input (print (" what is your age ")))
+if(age >= 18):
+    print(" you can vote")
 else:
-    print (" sorry you cannot vote")
+    print(" sorry you cannot vote")
     
