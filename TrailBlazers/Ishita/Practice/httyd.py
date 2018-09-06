27013358f9443b336d833b88ab7be65e556936be
ishitajain21 <40191155+ishitajain21@users.noreply.github.com>
2018-09-05 19:48:40 -0700

    Dragon Book

---
+++ TrailBlazers/Ishita/Practice/httyd.py
@@ -0,0 +1,12 @@
+# Ideas: 
+# ARrrange it again by class. Arrange in trainable dragons.
+
+print("Welcome to the Dragon Book. I will tell you about each.")
+print("http://howtotrainyourdragon.wikia.com/wiki/Category:Dragon_Species_from_the_Franchise")
+
+dragons = ["Armorwing", "Bewilderbeast", "Boneknapper", "Buffalord", "Catostrophic Quaken", "Cavern Crasher", "Changewing", "Crimson Goregutter", "Deadly Nadder", "Death Song", "Deathgripper", "Desert Wraith", "Devilish Dervish", "Dramillion", "Egg Biter", "Eruptdon", "Fireworm", "Flame Whipper", "Flightmare", "Foreverwing","Gobsucker", "Grapple Grounder", " Green Death", "Grim Gnasher", "Groncicle", "Gronckle", "Hackatoo", "Hideous Zippleback", "Hobgobbler", "Hobllegrunt","Hotburple", "" ]
+
+for i in range (len(dragons)):
+    print(dragons[i], " -- Enter " , i )
+
+
