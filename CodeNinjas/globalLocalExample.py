d2acb2d4ac4886d458e4d22b4af1f62e0c8d1ad7
shiv <shivprashant@outlook.com>
2018-09-08 11:23:46 -0700

    Ninja's 9_8 class

---
+++ CodeNinjas/globalLocalExample.py
@@ -0,0 +1,15 @@
+black_var = 10 # This is my global variable in the main function
+
+def initWordList():
+    orange_var = 20
+
+    print("initWordList : Print orange_var",orange_var)
+    print("initWordList: accessing black_var",black_var)
+
+initWordList()
+
+#print("main: accessing orange_var",orange_var)
+print("main: accessing black_var",black_var)
+
+
+
