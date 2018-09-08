d873cfdb383083bed1f8fd2b487533e6bad65a9a
shiv <shivprashant@outlook.com>
2018-09-08 13:49:48 -0700

    geeks class 9/8

--- The Geeks/myhangman.py
+++ The Geeks/myhangman.py
@@ -6,8 +6,10 @@
 
 #Define this as a global variable.
 listOfWords = ['skoolofcode', 'horses','redmond','puppies']
+NrOfTriesByUser = 0
 
 def init():
+    
     #Local variables v/s Global Variables
     print("init:Printing listofWorlds",listOfWords)
     return
@@ -24,10 +26,58 @@ def chooseAWord():
     #..
     return chWord
 
+# The createAWordWithDashes outputs a word with only the first n characters 
+# and rests hideen by dashes. Parameters to this function are 'word' and 'n' 
+# which is number of chars to display
+def createAWordWithDashes(word,n):
+    wordWithDashes = "Dum**"
+    #
+    #
+    #
+    return wordWithDashes
+
+#The function getUserGuess displays the word and takes user try as a input. The it
+# returns the user try as a result. It increments the number of tries.
+def getUserGuess(w):
+    print("The hideen word is ",w)
+    ret_value = input("Hey user! Guess this word")
+    global NrOfTriesByUser
+    NrOfTriesByUser = NrOfTriesByUser + 1
+    return ret_value
+
+def YouWin():
+    print("You have won! Great!")
+    return
+
+def YouLoose():
+    print("You loose")
+    return
+    
 #Main
 init()
 about()
-chooseAWord()
+word = chooseAWord()
+
+NrofCharShown = 3
+
+while(NrOfTriesByUser!=3):
+    wordToDisplay = createAWordWithDashes(word,NrofCharShown)
+    userGuess = getUserGuess(wordToDisplay)
+
+    if(userGuess == word):
+        YouWin()
+        exit()
+    
+    NrofCharShown = NrofCharShown + 1
+
+YouLoose()
+    
+
+
+#Use the word to display
+
+
+
 
 
 
