035eea8cbdcbda8618895464a5d7c2ce2df07775
ishitajain21 <40191155+ishitajain21@users.noreply.github.com>
2018-09-10 21:02:51 -0700

    FInd out which hat you belong to.

---
+++ TrailBlazers/Ishita/Practice/sortinghatquiz.py
@@ -0,0 +1,29 @@
+print("Welcome student to the Hogwarts School of Wizardy. I am the sorting hat.")
+
+g = 0
+h = 0
+r = 0
+s = 0 
+
+print(" Which one do you honor? enter the letter")
+print("a) Courage, Bravery, Nerve")
+print("b) Hard work, Patience, Jusctice, Loyalty")
+print("c) Intelligence, Creativity, Learning, Wit ")
+print("d) Ambition, Cunning, Leadership, Resourcefulness")
+
+a = input("")
+if a == "a": 
+    g = g+1
+else:
+    if a == " b":
+        h = h+1
+    else:
+        if a == "c":
+            r = r+1
+        else:
+            if a == "d":
+                s = s+1
+
+print("")
+
+                
