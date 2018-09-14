91063652a2ffe9a3c2860c89fb4e690b27b479ca
ishitajain21 <40191155+ishitajain21@users.noreply.github.com>
2018-09-13 20:25:58 -0700

    Create birthmonth.py

---
+++ TrailBlazers/Ishita/Practice/birthmonth.py
@@ -0,0 +1,67 @@
+print("Hi! I will tell you things about your birth month . WHat is it? (Enter the number)")
+a = input("")
+
+if a == 1:
+    print("Your birthstone is garnet and your birth flower is carnation.")
+else:
+    if a == 2:
+        print("Amethyst is your birthstone. Violet is your birth flower")
+    else:
+        if a == 3:
+            print("Aquamarine is your birthstone. Daffodil is your birth flower")
+        else:
+            if a == 4:
+                print("Diamond is your birthstone. Sweet pea is your flower.")
+            else:
+                if a == 5:
+                    print("EMerald is your birthstone. Lily of the valley is your flower.")
+                else:
+                    if a == 6:
+                        print("Alezandrite is your birthstone. Rose is your birthflower")
+                    else:
+                        if a== 7:
+                            print("Ruby is your birthstone. Larkspur is your birthflower.")
+                        else:
+                            if a== 8:
+                                print("Peridot is your birthstone. Gladiolous is your birthflower.")
+                            else:
+                                if a == 9:
+                                    print("Sapphire is your birthstone. Aster is your birthflower")
+                                else:
+                                    if a == 10:
+                                        print("Opal is your birthstone. Marigold is your birthflower")
+                                    else:
+                                        if a == 11:
+                                            print("Citrine is your birthstone. Chrysanthemum is your birthflower")
+                                        else:
+                                            if a == 12:
+                                                print("Blue Topaz is your birthstone. Narcissus is your birthflower")
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    
