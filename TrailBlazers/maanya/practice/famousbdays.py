9dc95700d81b9f67748e7850595faf198c3024ce
Maanya Arora <40130525+Maanya-Arora@users.noreply.github.com>
2018-09-15 10:26:14 -0700

    Maanya's Famous Birthday Dictionary

---
+++ TrailBlazers/maanya/practice/famousbdays.py
@@ -0,0 +1,28 @@
+def bday():
+	print("Hello! Enter the number of your birthmonth and I will tell you famous people who share your birthmonth!")
+	number = (input("Enter your birthmonth number:"))
+	if number == '1':
+		print("Some famous people born in January are Stephen Hawking, Alexander Hamilton, Richard Nixon, Ellen DeGeneres, Isaac Newton, and Zayn Malik. ")
+	if number == '2':
+		print("Some famous people born in February are Langston Hughes, Rosa Parks, Thomas Edison, Galileo Galilei, and George Washington. ")
+	if number == '3':
+		print("Some famous people born in March are Justin Bieber, Albert Einstien, Camilla Cabello, Alexandra Daddario, Vincent Van Gogh and Adam Levine. ")
+	if number == '4':
+		print("Some famous people born in April are Adolf Hitler, William Shakespere, Emma Watson, Kourtney Kardashian, Leonardo Da Vinci, and Thomas Jefferson. ")
+	if number == '5':
+		print("Some famous people born in May are Dwayne Johnson, John F. Kennedy, Mel B, Helena Bonham Carter, Adele, and Harry S. Turman.")
+	if number == '6':
+		print("Some famous people born in June are Anthony Bourdin, Lionel Messi, Donald Trump, Ariana Grande, Elon Musk, Kanye West and Marilyn Monroe. ")
+	if number == '7':
+		print("Some famous people born in July are Maanya Arora, Priyanka Chopra, Tom Cruise, Jennifer Lopez, Selena Gomez, Julius Ceaser and Frida Kahlo.")
+	if number == '8':
+		print("Some famous people born in August are John McCain, Demi Lovato, Michael Jackson, Kylie Jenner, Barak Obama, Dua Lipa, Shawn Mendes, and Jennifer Lawrence. ")
+	if number == '9':
+		print("Some famous people born in September are Narendra Modi, Avicci, Stephen King, Maddie Zeigler, Leo Tolstoy, William Howard Taft, and Zendaya. ")
+	if number == '10':
+		print("Some famous people born in October are Kim Kardashian, Mahatma Ghandi, Ryan Reynolds, Eminem, Zac Efron, Bill Gates, Pablo Picasso, and Simon Cowell.")
+	if number == '11':
+		print("Some famous people born in November are Kendall Jenner, Kris Jenner, Gordon Ramsey, Miley Cyrus, Shah Rukh Khan, Emma Stone, Shailene Woodly, and Karen Jillian.")
+	if number == '12':
+		print("Some famous people born in December are Kate Spade, LeBron James, Nikki Minaj, Taylor Swift, Britney Spears, Hailee Stienfield, Steven Spielberg, and Walt Disney.")
+bday()
